302054dcd163d56c3daa0ede240989d2100781ad
Md. Mehedi Hassan <mehedirony22@gmail.com>
2018-05-16 02:03:00 +0600

    Add solution for problem `A - Game` Link: https://codeforces.com/contest/984/problem/A Tags: sortings, *800 Ref: https://codeforces.com/contest/984/submission/38301956

---
+++ codeforces/984/A.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main()
+{
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++)
+    cin>>a[i];
+    sort(a,a+n);
+    if(n%2) cout<<a[n/2];
+    else cout<<a[n/2 -1];
+}
