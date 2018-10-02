cd1ed6f064f131449f91a595d1c5ee7c006c7fd6
Md. Mehedi Hassan <mehedirony22@gmail.com>
2018-10-03 04:06:00 +0600

    Add solution for problem `A - Is it rated?` Link: https://codeforces.com/contest/807/problem/A Tags: implementation, sortings, *900 Ref: https://codeforces.com/contest/807/submission/43706342

---
+++ codeforces/807/A.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+
+    int pre[n], aft[n];
+
+    for(int i=0;i<n;i++){
+        cin>>pre[i] >> aft[i];
+    }
+
+    int done = 0;
+    for(int i=0;i<n ;i++){
+        if(pre[i] != aft[i]){
+            cout<<"rated";
+            return 0;
+        }
+    }
+
+    for(int i=0;i<n-1;i++){
+        if(pre[i] < aft[i+1]){
+            cout<<"unrated";
+            return 0;
+        }
+    }
+
+    cout<<"maybe";
+}
