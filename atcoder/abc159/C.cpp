1d41d79da8f1e21ab6bb0599391b5e0efc3843bd
Md. Mehedi Hassan <mehedirony22@gmail.com>
2020-03-22 18:26:00 +0600

    Add solution for problem `C - Maximum Volume` Link: https://atcoder.jp/contests/abc159/tasks/abc159_c Tags: AtCoder, *300 Ref: https://atcoder.jp/contests/abc159/submissions/11105865

---
+++ atcoder/abc159/C.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+#define ll      long long
+#define pii     pair<int, int>
+#define f       first
+#define s       second
+
+using namespace std;
+int main()
+{
+    double l;
+    cin>>l;
+
+    l /= 3.0;
+    l = l*l*l;
+    printf("%.10f", l);
+}
+
+
