9bcd512d1792308eb04cbf4772ec9d596e56f156
Md. Mehedi Hassan <mehedirony22@gmail.com>
2020-04-01 20:48:00 +0600

    Add solution for problem `D - Again?` Link: https://codeforces.com/contest/1331/problem/D Tags: implementation Ref: https://codeforces.com/contest/1331/submission/75106741

---
+++ codeforces/1331/D.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+#define ll      long long
+#define pii     pair<int, int>
+#define f       first
+#define s       second
+#define sf(a) scanf("%d", &a)
+#define sfl(a) scanf("%lld", &a)
+#define pf(a) printf("%d ", a)
+#define pfl(a) printf("%lld\n", a)
+#define Case(a) printf("Case %d: ", a)
+
+using namespace std;
+int main()
+{
+    string s;
+    cin>>s;
+
+    if((s[6]-'0') % 2)
+        cout<<1;
+    else
+        cout<<0;
+}
+
