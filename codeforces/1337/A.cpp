0258122c1b1cf57491ecfca05844febcda49e776
Md. Mehedi Hassan <mehedirony22@gmail.com>
2020-04-15 20:48:00 +0600

    Add solution for problem `A - Ichihime and Triangle` Link: https://codeforces.com/contest/1337/problem/A Tags: constructive algorithms, math, *800 Ref: https://codeforces.com/contest/1337/submission/76824716

---
+++ codeforces/1337/A.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+#define ll      long long
+#define pii     pair<int, int>
+#define f       first
+#define s       second
+#define sf(a) scanf("%d", &a)
+#define sfl(a) scanf("%lld", &a)
+#define pf(a) printf("%d ", a)
+#define pfl(a) printf("%lld\n", a)
+#define Case(a) printf("Case %d: ", a)
+#define FASTIO ios_base::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+const double eps = 1e-9;
+const double pi = acos(-1);
+
+bool Equal(double x, double y)
+{
+    return fabs(x-y)<eps; ///x==y
+}
+
+bool Greater(double x, double y)
+{
+    return x-eps>y; ///x>y
+}
+
+bool Lesser(double x, double y)
+{
+    return x+eps<y; ///x>y
+}
+
+struct point
+{
+    double x, y, ang;
+};
+
+
+
+
+
+using namespace std;
+int main()
+{
+    FASTIO;
+    int t;
+    cin>>t;
+
+    ll a, b, c, d;
+
+    for(int tc = 1;tc<=t; tc++){
+        cin>>a>>b>>c>>d;
+        cout<<b<<" "<<c<<" "<<c<<endl;
+    }
+}
+
+
