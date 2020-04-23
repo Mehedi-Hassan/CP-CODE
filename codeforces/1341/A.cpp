022345e5a173491dbf7b93ba3544b2c188765f6a
Md. Mehedi Hassan <mehedirony22@gmail.com>
2020-04-23 20:50:00 +0600

    Add solution for problem `A - Nastya and Rice` Link: https://codeforces.com/contest/1341/problem/A Tags: math, *900 Ref: https://codeforces.com/contest/1341/submission/77770886

---
+++ codeforces/1341/A.cpp
@@ -0,0 +1,60 @@
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx,avx2,fma")
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define ll      long long
+#define pii     pair<int, int>
+#define f       first
+#define s       second
+#define sf(a) scanf("%d", &a)
+#define sfl(a) scanf("%lld", &a)
+#define pf(a) printf("%d ", a)
+#define pfl(a) printf("%lld\n", a)
+#define Case(a) printf("Case %d: ", a)
+#define FASTIO ios_base::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+
+const double eps = 1e-9;
+const double pi = acos(-1);
+
+bool Equal(double x, double y)
+{
+    return fabs(x-y)<eps; ///x==y
+}
+
+bool Greater(double x, double y)
+{
+    return x-eps>y; ///x>y
+}
+
+bool Lesser(double x, double y)
+{
+    return x+eps<y; ///x>y
+}
+
+struct point
+{
+    double x, y, ang;
+};
+
+
+
+int main()
+{
+    FASTIO;
+    int t;
+    cin>>t;
+
+    for(int tc = 1;tc<=t; tc++){
+        ll a, b, c, d, n;
+        cin>>n>>a>>b>>c>>d;
+
+        if( (a-b)*n>(c+d) || (a+b)*n < (c-d) )
+            cout<<"No\n";
+        else
+            cout<<"Yes\n";
+    }
+}
+
+
