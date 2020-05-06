c5465d8888dcf761b89e38d4348f3da0b8f5c7eb
Md. Mehedi Hassan <mehedirony22@gmail.com>
2020-05-07 02:30:00 +0600

    Add solution for problem `A - Phoenix and Balance` Link: https://codeforces.com/contest/1348/problem/A Tags: greedy, math, *800 Ref: https://codeforces.com/contest/1348/submission/79206728

---
+++ codeforces/1348/A.cpp
@@ -0,0 +1,76 @@
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx,avx2,fma")
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define ll              long long
+#define pii             pair<int, int>
+#define f               first
+#define s               second
+#define sf(a)           scanf("%d", &a)
+#define sfl(a)          scanf("%lld", &a)
+#define pf(a)           printf("%d ", a)
+#define pfl(a)          printf("%lld\n", a)
+#define Case(a)         printf("Case %d: ", a)
+#define FASTIO          ios_base::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+#define pb              push_back
+#define mp              make_pair
+#define mem(arr)        memset(arr, 0, sizeof(arr))
+#define mem1(arr)       memset(arr, -1, sizeof(arr))
+
+const double eps = 1e-9;
+const double pi = acos(-1);
+
+bool Equal(double x, double y)
+{
+    return fabs(x-y)<eps; ///x==y
+}
+
+bool Greater(double x, double y)
+{
+    return x-eps>y; ///x>y
+}
+
+bool Lesser(double x, double y)
+{
+    return x+eps<y; ///x>y
+}
+
+struct point
+{
+    double x, y, ang;
+};
+
+
+
+int main()
+{
+    FASTIO;
+
+    int t;
+    cin>>t;
+
+    for(int tc = 1;tc<=t; tc++){
+        ll n;
+        cin>>n;
+
+        ll x = 1, a = 0, b = 0;
+        for(int i=1;i<n/2;i++){
+            x*=2;
+            a += x;
+        }
+        for(int i=n/2;i<n;i++){
+            x*=2;
+            b += x;
+        }
+
+        x*=2;
+        a += x;
+
+        cout<<abs(a-b)<<endl;
+    }
+}
+
+
+
