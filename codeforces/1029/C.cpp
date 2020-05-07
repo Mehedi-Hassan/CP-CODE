c72e75389181e38fd32e272821011a3895327c69
Md. Mehedi Hassan <mehedirony22@gmail.com>
2020-05-08 02:07:00 +0600

    Add solution for problem `C - Maximal Intersection` Link: https://codeforces.com/contest/1029/problem/C Tags: greedy, math, sortings, *1600 Ref: https://codeforces.com/contest/1029/submission/79324156

---
+++ codeforces/1029/C.cpp
@@ -0,0 +1,89 @@
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx,avx2,fma")
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define ll              long long
+#define pii             pair<int, int>
+#define f               first
+#define s               second
+#define sf(a)           scanf("%d", &a)
+#define sfl(a)          scanf("%lld", &a)
+#define pf(a)           printf("%d ", a)
+#define pfl(a)          printf("%lld\n", a)
+#define Case(a)         printf("Case %d: ", a)
+#define FASTIO          ios_base::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+#define pb              push_back
+#define mp              make_pair
+#define mem(arr)        memset(arr, 0, sizeof(arr))
+#define mem1(arr)       memset(arr, -1, sizeof(arr))
+
+const double eps = 1e-9;
+const double pi = acos(-1);
+
+int dx[5] = {+1, +0, -1, -0};
+int dy[5] = {+0, +1, -0, -1};
+
+bool Equal(double x, double y)
+{
+    return fabs(x-y)<eps; ///x==y
+}
+
+bool Greater(double x, double y)
+{
+    return x-eps>y; ///x>y
+}
+
+bool Lesser(double x, double y)
+{
+    return x+eps<y; ///x>y
+}
+
+struct point
+{
+    double x, y, ang;
+};
+
+
+
+int main()
+{
+    FASTIO
+
+    int n;
+    cin>>n;
+
+    int s[n], e[n], a[n], b[n];
+
+    for(int i=0;i<n;i++){
+        cin>>s[i]>>e[i];
+        a[i] = s[i];
+        b[i] = e[i];
+    }
+
+    sort(s, s+n, greater<int>());
+    sort(e, e+n);
+
+    ll l = 1e10 , r = -1e10, ans = 0;
+    for(int i=0;i<n;i++){
+        if(a[i]==s[0]){
+            l = s[1];
+        }
+        else
+            l = s[0];
+        if(b[i] == e[0]){
+            r = e[1];
+        }
+        else
+            r = e[0];
+
+        ans = max(ans, r-l);
+//        cout
+    }
+
+    cout<<ans;
+}
+
+
+
