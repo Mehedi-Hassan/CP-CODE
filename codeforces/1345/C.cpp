5903821c3ac67b1b0542bbbf769ac18079fc0b5e
Md. Mehedi Hassan <mehedirony22@gmail.com>
2020-05-07 15:45:00 +0600

    Add solution for problem `C - Hilbert's Hotel` Link: https://codeforces.com/contest/1345/problem/C Tags: math, sortings, *1600 Ref: https://codeforces.com/contest/1345/submission/79255129

---
+++ codeforces/1345/C.cpp
@@ -0,0 +1,88 @@
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx,avx2,fma")
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define ll              long long
+#define pii             pair<int, int>
+#define f               first
+#define s               second
+#define sf(a)           scanf("%d", &a)
+#define sfl(a)          scanf("%lld", &a)
+#define pf(a)           printf("%d ", a)
+#define pfl(a)          printf("%lld\n", a)
+#define Case(a)         printf("Case %d: ", a)
+#define FASTIO          ios_base::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+#define pb              push_back
+#define mp              make_pair
+#define mem(arr)        memset(arr, 0, sizeof(arr))
+#define mem1(arr)       memset(arr, -1, sizeof(arr))
+
+const double eps = 1e-9;
+const double pi = acos(-1);
+
+int dx[5] = {+1, +0, -1, -0};
+int dy[5] = {+0, +1, -0, -1};
+
+bool Equal(double x, double y)
+{
+    return fabs(x-y)<eps; ///x==y
+}
+
+bool Greater(double x, double y)
+{
+    return x-eps>y; ///x>y
+}
+
+bool Lesser(double x, double y)
+{
+    return x+eps<y; ///x>y
+}
+
+struct point
+{
+    double x, y, ang;
+};
+
+
+map<int, int>cnt;
+
+int main()
+{
+    FASTIO
+
+    int t;
+    cin>>t;
+
+    for(int tc = 1;tc<=t; tc++){
+        int n;
+        cin>>n;
+
+        int a[n];
+        for(int i=0;i<n;i++){
+            cin>>a[i];
+        }
+
+        int id, d = 0;
+        for(int k=0;k<n;k++){
+            id = ((k%n)+n)%n;
+//            if(id<0) id += n;
+
+            id = k+a[id];
+            id = ((id%n) + n)%n;
+            cnt[id]++;
+            if(cnt[id] > 1){
+                d = 1;
+                break;
+            }
+        }
+
+        if(d == 0)
+            cout<<"YES\n";
+        else
+            cout<<"NO\n";
+
+        cnt.clear();
+    }
+}
