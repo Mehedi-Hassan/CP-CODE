b900544599d2e4883c2201e375e8c4df70695cf9
Md. Mehedi Hassan <mehedirony22@gmail.com>
2020-09-28 16:09:00 +0600

    Add solution for problem `E - Rock, Paper, Scissors` Link: https://codeforces.com/contest/1426/problem/E Tags: brute force, constructive algorithms, flows, greedy, math, *1800 Ref: https://codeforces.com/contest/1426/submission/94111970

---
+++ codeforces/1426/E.cpp
@@ -0,0 +1,183 @@
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx,avx2,fma")
+#include <algorithm>
+#include <bits/stdc++.h>
+#include <unordered_map>
+#include <unordered_set>
+
+using namespace std;
+
+#define PI              acos(-1.0)
+#define No              cout<<"No\n"
+#define Yes             cout<<"Yes\n"
+#define no              cout<<"no\n"
+#define yes             cout<<"yes\n"
+#define NO              cout<<"NO\n"
+#define YES             cout<<"YES\n"
+#define MOD             (int)1000000007
+#define int             long long
+#define ll              long long
+#define pii             pair<int, int>
+#define fi               first
+#define se              second
+#define sf(a)           scanf("%lld", &a)
+#define pf(a)           printf("%lld ", a)
+#define case(a)         cout<<"Case #"<<a<<":\n";
+#define FASTIO          ios_base::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+#define pb              emplace_back
+#define mp              make_pair
+#define mem(arr)        memset(arr, 0, sizeof(arr))
+#define mem1(arr)       memset(arr, -1, sizeof(arr))
+#define all(a)          a.begin(),a.end()
+#define f(i,a,b)        for (int i = a; i<=b; i++)
+#define fr(i,a,b)       for (int i = a; i>=b; i--)
+#define rr              return 0
+#define prec(n)         fixed<<setprecision(n)
+#define maxpq           priority_queue<int>
+#define minpq           priority_queue<int, vector<int>, greater<int> >
+#define inf             (int)(1e18)
+#define vi              vector<int>
+#define endl            "\n"
+#define vii             vector<pii>
+#define sz(s)           s.size()
+#define bits(n)         __builtin_popcount(n)
+#define lcm(a,b)        (a*(b/gcd(a,b)))
+#define MX              500005
+
+
+int gcd(int a, int b)   {return __gcd(a,b);}
+
+inline void normal(ll &a) { a %= MOD; (a < 0) && (a += MOD); }
+inline ll modMul(ll a, ll b) { a %= MOD, b %= MOD; normal(a), normal(b); return (a*b)%MOD; }
+inline ll modAdd(ll a, ll b) { a %= MOD, b %= MOD; normal(a), normal(b); return (a+b)%MOD; }
+inline ll modSub(ll a, ll b) { a %= MOD, b %= MOD; normal(a), normal(b); a -= b; normal(a); return a; }
+inline ll modPow(ll b, ll p) { ll r = 1; while(p) { if(p & 1LL) r = modMul(r, b); b = modMul(b, b); p >>= 1LL; } return r; }
+inline ll modInverse(ll a) { return modPow(a, MOD-2); }
+inline ll modDiv(ll a, ll b) { return modMul(a, modInverse(b)); }
+
+inline bool checkBit(ll n, int i) { return n&(1LL<<i); }
+inline ll setBit(ll n, int i) { return n or (1LL<<i);; }
+inline ll resetBit(ll n, int i) { return n&(~(1LL<<i)); }
+
+
+const double eps = 1e-9;
+
+int dx[5] = {+1, +0, -1, -0};
+int dy[5] = {+0, +1, -0, -1};
+int XX[] = { -1, -1, -1, 0, 0, 1, 1, 1 };
+int YY[] = { -1, 0, 1, -1, 1, -1, 0, 1 };
+
+bool Equal(double x, double y)
+{
+    return fabs(x-y)<eps; ///x==y
+}
+
+bool Greater(double x, double y)
+{
+    return x-eps>y; ///x>y
+}
+
+bool Lesser(double x, double y)
+{
+    return x+eps<y; ///x<y
+}
+
+struct point
+{
+    double x, y, ang;
+};
+
+#define error(args...) { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); err(_it, args); cerr << '\n'; }
+void err(istream_iterator<string> it) {}
+template<typename T, typename... Args> void err(istream_iterator<string> it, T a, Args... args) { cerr << *it << " = " << a << " "; err(++it, args...); }
+
+///Write what you need -->
+/// nt, ft, kmp, tri, sufarr, ub_lb, lis
+/// dsu
+
+
+///Template Ends Here////////////////////////////
+
+
+signed main()
+{
+//    root = new trinode();
+//    freopen("input.txt", "r", stdin);
+//    freopen("output.txt", "w", stdout);
+
+//    FASTIO
+    int T;
+//    cin>>T;
+    T = 1;
+
+    for(int tc = 1;tc<=T; tc++){
+//        int n;
+//        cin>>n;
+//
+//        int a[n+1];
+//        f(i, 1, n){
+//            cin>>a[i];
+//        }
+
+        int n;
+        cin>>n;
+
+        int aa[4], bb[4];
+        cin>>aa[1]>>aa[2]>>aa[3];
+        cin>>bb[1]>>bb[2]>>bb[3];
+
+        vi a;
+        a.pb(11);
+        a.pb(12);
+        a.pb(13);
+        a.pb(21);
+        a.pb(22);
+        a.pb(23);
+        a.pb(31);
+        a.pb(32);
+        a.pb(33);
+
+        int ans1 = inf, ans2 = -inf;
+        do{
+            int ar = aa[1], as = aa[2], ap = aa[3], tot = 0;
+            int br = bb[1], bs = bb[2], bp = bb[3];
+            f(i, 0, 9-1){
+                int mn = min(aa[a[i]/10], bb[a[i]%10]);
+                aa[a[i]/10] -= mn;
+                bb[a[i]%10] -= mn;
+
+                int l = a[i]/10;
+                int r = a[i]%10;
+
+                if(l==1 && r==2)
+                    tot += mn;
+                if(l==2 && r==3)
+                    tot += mn;
+                if(l==3 && r==1)
+                    tot += mn;
+            }
+            ans1 = min(ans1, tot);
+            ans2 = max(ans2, tot);
+
+            aa[1] = ar, aa[2] = as, aa[3] = ap;
+            bb[1] = br, bb[2] = bs, bb[3] = bp;
+
+        }while(next_permutation(all(a)));
+
+        cout<<ans1<<" "<<ans2<<endl;
+        rr;
+
+
+
+
+
+    }
+
+
+//    tridel(root);
+}
+
+
+
+
+
